563f08be13a9b124c56afd27c76d36457fdb6738
Nayem Mufakkharul <mufakkharul.nayem@rakuten.com>
2024-12-03 16:02:50 +0900

    Move `RemoteFeedLoader` to production module

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Nayem, Mufakkharul | Nil | GSSD on 2024/12/03.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let client: HTTPClient
+    private let url: URL
+    
+    public init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
