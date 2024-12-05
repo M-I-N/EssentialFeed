a231470471dcdaefcb7b28da696228f53462ef1b
Nayem Mufakkharul <mufakkharul.nayem@rakuten.com>
2024-12-05 16:03:58 +0900

    Use explicit JSON data for non 200 HTTP response test to account for HTTP response status code in production

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -51,7 +51,8 @@ class RemoteFeedLoaderTests: XCTestCase {
         
         samples.enumerated().forEach { index, code in
             expect(sut: sut, toCompleteWith: .failure(.invalidData)) {
-                client.complete(withStatusCode: code, at: index)
+                let json = makeItemsJSON(items: [])
+                client.complete(withStatusCode: code, data: json, at: index)
             }
         }
     }
@@ -166,7 +167,7 @@ class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
