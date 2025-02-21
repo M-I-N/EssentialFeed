ae5b42a1cfb8a0b2bee27d134d61165541c8bbdd
Nayem Mufakkharul <mufakkharul.nayem@rakuten.com>
2025-02-21 18:49:56 +0900

    Rename `RemoteFeedLoaderTests` to reflect the use case it refers to - Load Feed From Remote Use Case

--- EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -1,5 +1,5 @@
 //
-//  RemoteFeedLoaderTests.swift
+//  LoadFeedFromRemoteUseCaseTests.swift
 //  EssentialFeedTests
 //
 //  Created by Nayem, Mufakkharul | Nil | GSSD on 2024/12/02.
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class RemoteFeedLoaderTests: XCTestCase {
+class LoadFeedFromRemoteUseCaseTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
