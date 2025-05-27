8f0064b4fb0acddda1fc8bba27d236eb13bd01b6
Nayem Mufakkharul <mufakkharul.nayem@rakuten.com>
2025-05-27 16:18:30 +0900

    Move 'FeedStore' to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Nayem, Mufakkharul | Nil | GSSD on 2025/05/27.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
