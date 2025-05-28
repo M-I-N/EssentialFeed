915712ac0768fe4c4e7e0d3c30a5149d8d04236e
Nayem Mufakkharul <mufakkharul.nayem@rakuten.com>
2025-05-28 17:02:33 +0900

    Add 'RemoteFeedItem' data transfer representation to decouple the 'FeedItemsMapper' from 'FeedItem' data models

--- EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -7,35 +7,22 @@
 
 import Foundation
 
+struct RemoteFeedItem: Decodable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+}
+
 enum FeedItemsMapper {
     private struct Root: Decodable {
-        let items: [Item]
-        
-        var feed: [FeedItem] {
-            items.map { $0.item }
-        }
-    }
-
-    private struct Item: Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-        
-        var item: FeedItem {
-            FeedItem(
-                id: id,
-                description: description,
-                location: location,
-                imageURL: image
-            )
-        }
+        let items: [RemoteFeedItem]
     }
     
     private static func map(
         _ data: Data,
         _ response: HTTPURLResponse
-    ) throws -> [FeedItem] {
+    ) throws -> [RemoteFeedItem] {
         guard response.statusCode == 200 else {
             throw RemoteFeedLoader.Error.invalidData
         }
@@ -45,18 +32,14 @@ enum FeedItemsMapper {
             from: data
         )
         
-        return root.feed
+        return root.items
     }
     
     static func map(
         data: Data,
         byChecking response: HTTPURLResponse
-    ) -> RemoteFeedLoader.Result {
-        do {
-            let items = try FeedItemsMapper.map(data, response)
-            return .success(items)
-        } catch {
-            return .failure(RemoteFeedLoader.Error.invalidData)
-        }
+    ) throws -> [RemoteFeedItem] {
+        let items = try FeedItemsMapper.map(data, response)
+        return items
     }
 }
